63acce168b0e32c788660203be09f250da41cc1d
AlexandrSybirko <63520677+AlexandrSybirko@users.noreply.github.com>
2020-10-14 18:23:27 +0300

    Создает шаблон

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './styles.css';
+import menuTpl from './templates/menu-markup.hbs';
+import menu from './menu.json';
+
+const theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+// делаем разметку
+const galleryMenu = document.querySelector('ul.js-menu');
+
+const markupMenu = menuTpl(menu);
+galleryMenu.insertAdjacentHTML('beforeend', markupMenu);
